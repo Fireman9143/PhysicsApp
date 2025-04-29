4bdf38e70da227af132385e1d806469348c15152
Fireman9143 <168347053+Fireman9143@users.noreply.github.com>
2025-04-29 17:15:57 -0500

    Add files via upload

---
+++ greekletters.py
@@ -0,0 +1,29 @@
+import matplotlib
+
+greek_letters = {'alpha': ['\u0391', '\u03B1'], 
+                 'beta': ['\u0392', '\u03B2'], 
+                 'gamma': ['\u0393', '\u03B3'],
+                 'delta': ['\u0394', '\u03B4'],
+                 'epsilon': ['\u0395', '\u03B5'],
+                 'zeta': ['\u0396', '\u03B6'],
+                 'eta': ['\u0397', '\u03B7'],
+                 'theta': ['\u0398', '\u03B8'],
+                 'iota': ['\u0399', '\u03B9'],
+                 'kappa': ['\u039A', '\u03BA'],
+                 'lambda': ['\u039B', '\u03BB'],
+                 'mu': ['\u039C', '\u03BC'],
+                 'nu': ['\u039D', '\u03BD'],
+                 'xi': ['\u039E', '\u03BE'],
+                 'omicron': ['\u039F', '\u03BF'],
+                 'pi': ['\u03A0', '\u03C0'],
+                 'rho': ['\u03A1', '\u03C1'],
+                 'sigma': ['\u03A3', '\u03C3'],
+                 'tau': ['\u03A4', '\u03C4'],
+                 'upsilon': ['\u03A5', '\u03C5'],
+                 'phi': ['\u03A6', '\u03C6'],
+                 'chi': ['\u03A7', '\u03C7'],
+                 'psi': ['\u03A8', '\u03C8'],
+                 'omega': ['\u03A9', '\u03C9'],
+                 }
+
+print(greek_letters)
